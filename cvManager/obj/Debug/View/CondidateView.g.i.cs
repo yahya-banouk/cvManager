5939a9676e449e5d73fadb5d681e3c3540d327e7
yahya banouk <yahya.banouk@uit.ac.ma>
2022-09-08 14:36:10 +0100

    CR configuration

--- cvManager/obj/Debug/View/CondidateView.g.i.cs
+++ cvManager/obj/Debug/View/CondidateView.g.i.cs
@@ -10,6 +10,7 @@
 //------------------------------------------------------------------------------
 
 using FontAwesome.Sharp;
+using RootLibrary.WPF.Localization;
 using System;
 using System.Diagnostics;
 using System.Windows;
--- cvManager/obj/Release/View/LoginView.g.cs
+++ cvManager/obj/Release/View/LoginView.g.cs
@@ -9,6 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using RootLibrary.WPF.Localization;
 using System;
 using System.Diagnostics;
 using System.Windows;
